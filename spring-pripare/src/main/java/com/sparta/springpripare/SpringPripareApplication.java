07b29bf81dd21b0942b689b989bc37ecb29a319e
weiß Yi <78130010+Rosa-Eau@users.noreply.github.com>
2024-02-20 18:08:31 +0900

    Day16

---
+++ spring-pripare/src/main/java/com/sparta/springpripare/SpringPripareApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springpripare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPripareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPripareApplication.class, args);
+    }
+
+}
---
+++ spring-pripare/src/test/java/com/sparta/springpripare/SpringPripareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springpripare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPripareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
