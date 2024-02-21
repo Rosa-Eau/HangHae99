fd6dd9f943be66c9625780f872eabcbc2e2f4b66
weiß Yi <78130010+rosa-eau@users.noreply.github.com>
2024-02-21 17:53:54 +0900

    springMVC

---
+++ springLearn/spring-mvc/src/main/java/org/sparta/springmvc/controller/HelloController.java
@@ -0,0 +1,33 @@
+package org.sparta.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/get")
+    @ResponseBody
+    public String get(){
+        return "GET Method Request";
+    }
+
+    @PostMapping("/post")
+    @ResponseBody
+    public String post(){
+        return "POST Method Request";
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public String delete(){
+        return "DELETE Method Request";
+    }
+
+}
