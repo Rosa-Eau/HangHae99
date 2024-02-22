9226f8d5970268cefe03a370b3df56b9d4061076
weiß Yi <78130010+rosa-eau@users.noreply.github.com>
2024-02-22 19:19:49 +0900

    :sparkles: JDBCTemplate

--- springLearn/memo/src/main/java/org/sparta/memo/dto/MemoResponseDto.java
+++ springLearn/memo/src/main/java/org/sparta/memo/dto/MemoResponseDto.java
@@ -5,13 +5,19 @@ import org.sparta.memo.entity.Memo;
 
 @Getter
 public class MemoResponseDto {
-    private final Long id;
-    private final String username;
-    private final String contents;
+    private  Long id;
+    private  String username;
+    private  String contents;
 
     public MemoResponseDto(Memo memo) {
         this.id = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
+    }
+
+    public MemoResponseDto(Long id, String username, String contents) {
+        this.id = id;
+        this.username = username;
+        this.contents = contents;
     }
 }
