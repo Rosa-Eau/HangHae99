eaf8591f033ac3d9f9a86b6c1835e098d90fe68e
weiß Yi <78130010+rosa-eau@users.noreply.github.com>
2024-02-22 16:11:11 +0900

    request

--- springLearn/spring-mvc/src/main/java/org/sparta/springmvc/request/Star.java
+++ springLearn/spring-mvc/src/main/java/org/sparta/springmvc/request/Star.java
@@ -1,12 +1,10 @@
 package org.sparta.springmvc.request;
 
+import lombok.Setter;
+
+@Setter
 public class Star {
 
     String name;
     int age;
-
-    public Star(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
 }
