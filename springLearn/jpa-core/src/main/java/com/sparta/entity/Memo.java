3e15b4474761ccbb7a51b6ce445bae5b26a3d9d7
weiß Yi <78130010+rosa-eau@users.noreply.github.com>
2024-02-25 23:32:19 +0900

    entity 이해하기

--- springLearn/jpa-core/src/main/java/com/sparta/entity/Memo.java
+++ springLearn/jpa-core/src/main/java/com/sparta/entity/Memo.java
@@ -1,14 +1,11 @@
 package com.sparta.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Table(name = "memo") // 매핑할 테이블의 이름을 지정
 public class Memo {
-    @Id
+    @Id // Primary key
     private Long id;
 
     // nullable: null 허용 여부
@@ -19,4 +16,28 @@ public class Memo {
     // length: 컬럼 길이 지정
     @Column(name = "contents", nullable = false, length = 500)
     private String contents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
 }
