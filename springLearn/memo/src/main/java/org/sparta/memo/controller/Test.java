d940c1736a74048eaa417fe0969709edf0398699
weiß Yi <78130010+rosa-eau@users.noreply.github.com>
2024-02-25 14:52:11 +0900

    :recycle: 객체 중복 생성 문제 해결

---
+++ springLearn/memo/src/main/java/org/sparta/memo/controller/Test.java
@@ -0,0 +1,16 @@
+package org.sparta.memo.controller;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Integer count = 1;
+        boolean result = false;
+
+        if (count.equals(Integer.valueOf(1))) {
+           result = true;
+        }
+
+        System.out.println(result);
+    }
+}
---
+++ springLearn/memo/src/main/java/org/sparta/memo/service/MemoService.java
@@ -0,0 +1,52 @@
+package org.sparta.memo.service;
+
+import org.sparta.memo.dto.MemoRequestDto;
+import org.sparta.memo.dto.MemoResponseDto;
+import org.sparta.memo.entity.Memo;
+import org.sparta.memo.repository.MemoRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class MemoService {
+
+    private final MemoRepository memoRepository;
+
+    public MemoService(JdbcTemplate jdbcTemplate) {
+        this.memoRepository = new MemoRepository(jdbcTemplate);
+    }
+
+
+    public MemoResponseDto createMemo(MemoRequestDto requestDto) {
+        // RequestDto -> Entity
+        Memo memo = new Memo(requestDto);
+
+        // DB 저장
+        Memo saveMemo = memoRepository.save(memo);
+
+        // Entity -> ResponseDto
+        return new MemoResponseDto(memo);
+    }
+
+    public List<MemoResponseDto> getMemos() {
+        // DB 조회
+        return memoRepository.findAll();
+    }
+
+    public Long updateMemo(Long id, MemoRequestDto requestDto) {
+        // 해당 메모가 DB에 존재하는지 확인
+        return memoRepository.update(id, requestDto);
+    }
+
+    public Long deleteMemo(Long id) {
+        // 해당 메모가 DB에 존재하는지 확인
+       return memoRepository.delete(id);
+    }
+
+
+
+
+
+}
+
+
